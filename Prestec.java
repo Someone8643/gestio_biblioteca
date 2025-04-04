0bfc9529611321c06f4dd0a49397ec79250efddf
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-04-04 20:16:17 +0200

    Continuar Prestec.

--- Prestec.java
+++ Prestec.java
@@ -1,4 +1,46 @@
+import java.time.LocalDate;
+
 public class Prestec {
 
+    // // Propietats
 
+    private Usuari usuari;
+    private Llibre llibre;
+    private LocalDate dataPrestec;
+    private LocalDate dataRetorn;
+
+
+    // // Constructors
+
+    public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
+        this.usuari = usuari;
+        this.llibre = llibre;
+        this.dataPrestec = dataPrestec;
+
+        // De normal hi ha dues setmanes en un préstec
+        this.dataRetorn = dataPrestec.plusWeeks(2);
+    }
+
+
+    // // Mètodes
+
+    // Mètode per retornar el llibre del préstec
+    public void retornarLlibre() {
+
+
+    }
+
+    // // Getters i Setters
+
+    public Usuari getUsuari() {
+        return usuari;
+    }
+
+    public Llibre getLlibre() {
+        return llibre;
+    }
+
+    public LocalDate getDataRetorn() {
+        return dataRetorn;
+    }
 }
