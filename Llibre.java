bdc5df28117ec36665fdc768404e30c98ed880b9
Hector-Torres-D <hector.torres@insbaixcamp.cat>
2025-04-04 19:46:31 +0200

    Llibre creat

--- Llibre.java
+++ Llibre.java
@@ -1,5 +1,30 @@
 public class Llibre {
 
+// Propieades
+
+private String titol;
+private String autor;
+private boolean prestat;
+
+// Constructores
+
+public Llibre(String titol, String autor, boolean prestat){
+
+    this.titol = titol;
+    this.autor = autor;
+    this.prestat = false;
+}
+
+// Métodos
+
+getters y setters
+public String getTitol() {
+    return titol;
+}
+public String getAutor() {
+    return autor;
+}
+
 
 
 }
