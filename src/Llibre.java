008ffddaae5f04318a05ec16d6e65326a06bd019
Joel <joel.rodriguez@insbaixcamp.cat>
2025-04-04 19:01:07 +0200

    Commit inicial

---
+++ src/Llibre.java
@@ -0,0 +1,24 @@
+
+public class Llibre  {
+
+	private String titol = "Samuel";
+	private String autor = "Edgar";
+	public String getTitol() {
+		return titol;
+	}
+
+	public void setTitol(String titol) {
+		this.titol = titol;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Llibre llibre1 = new Llibre();
+		ArrayList<Llibre> llibres = new ArrayList();
+		llibres.add(llibre1);
+		Biblioteca biblioteca = new Biblioteca(llibres);
+		
+		biblioteca.llistarLlibres();
+	}
+
+}
