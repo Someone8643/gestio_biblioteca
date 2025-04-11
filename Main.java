999361eed80087bf8150d7bf447a1d0bff2dfe6b
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-04-11 19:30:32 +0200

    Afegir un menu (buit) al Main.

--- Main.java
+++ Main.java
@@ -18,5 +18,49 @@ public class Main {
 
         // Fer préstecs
         gestor.prestarLlibre(usuari, llibre1);
+
+
+        /*
+         * Bloc de codi per a copiar que mostra un menu i segons el número d'opció es fa un cas o altre.
+         * Pensat per a un ús general. Inclou Scanner.
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Benvingut al sistema!");
+
+        int opcio = 0;
+        while (opcio != -1) {
+
+            // Es mostra un menú, editar segons les necessitats
+            System.out.println("Opcions:");
+            System.out.println("1. .");
+            System.out.println("2. .");
+            System.out.println("3. .");
+            System.out.print("Introdueix un número d'opció (-1 per sortir): ");
+            opcio = entrada.nextInt();
+
+            // Posar codi (cridar funcions per exemple) de cada opció
+            switch (opcio) {
+                case 1: {
+
+
+                }
+                case 2: {
+
+
+                }
+                case 3: {
+
+
+                }
+                case -1: {
+
+                    System.out.println("Sortint del sistema...");
+                }
+
+                default:
+                    System.out.println("Opció incorrecta!");
+            }
+        } // Fí bucle
     }
 }
