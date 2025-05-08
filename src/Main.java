1b40ed05fd24f0470090547cee7c6fa1f9ee067a
Someone8643 <oriol.casas@insbaixcamp.cat>
2025-05-08 19:25:43 +0200

    Continuar menú

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Biblioteca biblioteca = new Biblioteca();
+        //Biblioteca biblioteca = new Biblioteca();
 
         // Llibres
-        Llibre llibre1 = new Llibre("1984", "George Orwell");
-        Llibre llibre2 = new Llibre("El petit príncep", "Antoine de Saint-Exupéry");
+        //Llibre llibre1 = new Llibre("1984", "George Orwell");
+        //Llibre llibre2 = new Llibre("El petit príncep", "Antoine de Saint-Exupéry");
 
-        biblioteca.afegirLlibre(llibre1);
-        biblioteca.afegirLlibre(llibre2);
+        //biblioteca.afegirLlibre(llibre1);
+        //biblioteca.afegirLlibre(llibre2);
 
         // Usuaris
-        Usuari usuari = new Usuari("Carla");
+        //Usuari usuari = new Usuari("Carla");
 
         // Crear el gestor
-        GestorBiblioteca gestor = new GestorBiblioteca();
+        //GestorBiblioteca gestor = new GestorBiblioteca();
 
         // Fer préstecs
-        gestor.prestarLlibre(usuari, llibre1);
+        //gestor.prestarLlibre(usuari, llibre1);
 
 
         /*
@@ -70,7 +70,7 @@ public class Main {
 
                                 Llibre llibreAfegir = new Llibre(titol, autor, false);
 
-                                gestor.afegirLlibre(llibreAfegir);
+                                //gestor.afegirLlibre(llibreAfegir);
                             }
                             case 2: {
 
@@ -78,7 +78,7 @@ public class Main {
                                 String titolABuscar = entrada.nextLine();
 
                                 // TODO
-                                Llibre llibreAModificar = gestor.buscarLlibreAccents(titolABuscar);
+                                //Llibre llibreAModificar = gestor.buscarLlibreAccents(titolABuscar);
 
                                 System.out.println("Introdueix les dades que tindrà el llibre:");
 
@@ -90,14 +90,14 @@ public class Main {
 
                                 Llibre llibreAfegir = new Llibre(titol, autor, false);
 
-                                gestor.afegirLlibre(llibreAfegir);
+                                //gestor.afegirLlibre(llibreAfegir);
                             }
                             case 3: {
 
                                 System.out.println("Eliminar llibre:");
 
                                 System.out.println("Introdueix el títol del llibre a ser eliminat:");
-                                gestor.eliminarLlibreNom(entrada.nextLine());
+                                //gestor.eliminarLlibreNom(entrada.nextLine());
                             }
                             case -1: {
 
@@ -108,14 +108,79 @@ public class Main {
                                 System.out.println("Opció incorrecta!");
                         }
                     }
-
-
-
-
                 }
                 case 2: {
+                    while (opcio != -1) {
+
+                        // Es mostra un menú, editar segons les necessitats
+                        System.out.println("Opcions:");
+                        System.out.println("1. Afegir usuari.");
+                        System.out.println("2. Modificar usuari.");
+                        System.out.println("3. Eliminar usuari.");
+                        System.out.println("4. Llistar usuaris.");
+                        System.out.println("5. Cercar usuari.");
+                        System.out.print("Introdueix un número d'opció (-1 per tornar enrere): ");
+                        opcio = entrada.nextInt();
+
+                        // Posar codi (cridar funcions per exemple) de cada opció
+                        switch (opcio) {
+                            case 1: {
+
+                                System.out.println("Introdueix les dades per crear un nou usuari:");
+
+                                System.out.println("a:");
+                                String a = entrada.nextLine();
+
+                                System.out.println("b:");
+                                String b = entrada.nextLine();
 
 
+                            }
+                            case 2: {
+
+                                System.out.println("Introdueix el nom del usuari per editar:");
+                                String usuariABuscar = entrada.nextLine();
+
+                                System.out.println("Introdueix les dades que tindrà l'usuari:");
+
+                                System.out.println("a:");
+                                String a = entrada.nextLine();
+
+                                System.out.println("b:");
+                                String b = entrada.nextLine();
+
+                            }
+                            case 3: {
+
+                                System.out.println("Eliminar usuari:");
+
+                                System.out.println("Introdueix el nom del usuari a ser eliminat:");
+                                String nomUsuari = entrada.nextLine();
+
+                            }
+                            case 4: {
+
+                                System.out.println("Llistar usuaris:");
+
+
+                            }
+                            case 5: {
+
+                                System.out.println("Cercar usuari:");
+
+                                System.out.println("Introdueix el nom del usuari a ser cercat:");
+                                String usuariACercar = entrada.nextLine();
+
+                            }
+                            case -1: {
+
+                                System.out.println("Tornant al menú general...");
+                            }
+
+                            default:
+                                System.out.println("Opció incorrecta!");
+                        }
+                    }
                 }
                 case -1: {
 
